c4e8b36b29f0578b80f19d4fcb4cb5632e1a2a3a
Oscar Guajardo <oscarguajardo@unap.cl>
2020-12-21 23:40:47 -0300

    primeras rutas del componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,72 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CardNewsComponent } from './components/card-news/card-news.component';
 import { LatestNewsComponent } from './components/latest-news/latest-news.component';
+import { CategoryNewsComponent } from './components/category-news/category-news.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FavouritesComponent } from './components/favourites/favourites.component';
+import { LogoutComponent } from './components/logout/logout.component';
+
+const rutas: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'LatestNews'
+  },
+  {
+    path: 'LatestNews',
+    component: LatestNewsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'favourites',
+    component: FavouritesComponent
+  },
+  {
+    path: 'technology',
+    component: CategoryNewsComponent
+  },
+  {
+    path: 'programming',
+    component: CategoryNewsComponent
+  },
+  {
+    path: 'game',
+    component: CategoryNewsComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CardNewsComponent,
-    LatestNewsComponent
+    LatestNewsComponent,
+    CategoryNewsComponent,
+    LoginComponent,
+    RegisterComponent,
+    FavouritesComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas, {
+      useHash: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/category-news/category-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-news',
+  templateUrl: './category-news.component.html',
+  styleUrls: ['./category-news.component.css']
+})
+export class CategoryNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
